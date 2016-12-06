58f7d7d70891266898ad3157118e071a63912986
Allen Nelson <anelson@narrativescience.com>
2016-12-06 23:30:13 +0000

    implement nar -> export conversion

--- src/servenix/common/narinfo.py
+++ src/servenix/common/narinfo.py
@@ -1,6 +1,12 @@
 """A python embedding of a NarInfo object."""
 import os
+from io import BytesIO
 import yaml
+
+# Magic 8-byte number that comes at the beginning of the export's bytes.
+EXPORT_INITIAL_MAGIC = b"\x01" + (b"\x00" * 7)
+# Magic 8-byte number that comes after the NAR, before export metadata.
+EXPORT_METADATA_MAGIC = b"NIXE\x00\x00\x00\x00"
 
 class NarInfo(object):
     def __init__(self, store_path, url, compression,
@@ -82,6 +88,79 @@ class NarInfo(object):
         store_dir, basename = os.path.split(self.store_path)
         references = [r for r in self.references if circular or r != basename]
         return [os.path.join(store_dir, r) for r in references]
+
+    def convert_nar_to_export(self, nar_bytes):
+        """Add metadata to a nar bytestring, turning it into an export.
+
+        A nix export is a nix archive coupled with some
+        metadata. Specifically, it adds information about references
+        and optionally deriver. This information is contained in a
+        NarInfo, so we can augment a nar here.
+
+        Nix exports are a binary format. The input to this function is
+        a bytestring intended to have been created from a call to
+        `nix-store --dump`, or equivalently, as returned by a nix
+        binary cache. The logic of this function adds a few things:
+
+        * An 8-byte magic header, which nix-store reads when it imports.
+        * The bytes of the NAR itself.
+        * Another magic bytestring, which is 'NIXE' followed by four nulls.
+        * The path to the object in the nix store being imported.
+        * The number of references.
+        * The path of each reference.
+        * The deriver path, if known (else an empty string).
+        * 8 empty bytes, to indicate we're not including a signature.
+        * 8 empty bytes, for reasons unknown to me but needed by nix-store.
+
+        Each string referenced above (e.g. paths) is represented by
+        first writing its length as an integer encoded in
+        little-endian 8 bytes, then the string itself, and then as
+        many null bytes as are needed to get to the nearest multiple
+        of 8 bytes. So for example, the string "hello" would be
+        represented as
+
+          "\x05\x00\x00\x00\x00\x00\x00\x00hello\x00\x00\x00"
+
+        Note that there are three zeros following the "hello" text, in
+        order to pad it to eight bytes.
+        """
+        # We'll build the string using a BytesIO for efficiency
+        bio = BytesIO()
+        def addstr(bstring):
+            """Utility function, adds a string with padding to the bytes."""
+            _len = len(bstring)
+            bio.write(_len.to_bytes(8, "little"))
+            bio.write(bstring)
+            if _len % 8 != 0:
+                bio.write(bytes(8)[:8 - (_len % 8)])
+
+        # Start with the magic header and nar bytes.
+        bio.write(EXPORT_INITIAL_MAGIC)
+        bio.write(nar_bytes)
+
+        # Write the magic value for the metadata.
+        bio.write(EXPORT_METADATA_MAGIC)
+
+        # Write the store path of the object.
+        addstr(self.store_path.encode("utf-8"))
+
+        # Write the number of references, and each reference.
+        refs = self.fullpath_references(circular=True)
+        bio.write(len(refs).to_bytes(8, "little"))
+        for ref in refs:
+            addstr(ref.encode("utf-8"))
+
+        if self.deriver is not None:
+            addstr(self.deriver.decode("utf-8"))
+        else:
+            addstr(b"")
+
+        # Add a 0 to indicate no signature, and then another 0 (not sure why).
+        bio.write((0).to_bytes(8, "little"))
+        bio.write((0).to_bytes(8, "little"))
+
+        # Return the contents of the bytesio as the resulting bytestring.
+        return bio.getvalue()
 
     @classmethod
     def from_dict(cls, dictionary):
