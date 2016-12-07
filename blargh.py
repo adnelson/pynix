112687af5c9041b5cd83e9fc7e7fd40af2830785
Allen Nelson <anelson@narrativescience.com>
2016-12-07 00:34:58 +0000

    remove unnecessary fetch.py

--- blargh.py
+++
@@ -1,8 +0,0 @@
-import os
-from servenix.client.sendnix import StoreObjectSender
-
-path = "/nix/store/aa9asgngyisnfcxk3hcqlpy8qcapf1lg-hello-2.10"
-
-sender = StoreObjectSender(os.environ["NIX_REPO_HTTP"])
-
-sender.get_references(path)
